d7a727b5207accba516beabb7ea7e8bb83db3bcf
Lami47 <liamrosenberg4777@gmail.com>
2024-10-23 10:16:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataset with a specific encoding
-file_path = 'Gym.csv'  # Replace with your CSV file path
-df = pd.read_csv(file_path)  # Change encoding as necessary
+file_path = 'Gym.csv'  # Gym.csv can be replace with your CSV file path
+df = pd.read_csv(file_path)  # allows the file to be read and used in python
 
 
-# Displays the first few rows of the datafile
+# Displays the first 5 rows of the datafile
 print("Initial Data:")
-print(df.head(5)) # to only display the 1st 5 rows
+print(df.head(5)) 
 
 # Identifies the different data types
 print("\nData Types:")
-print(df.dtypes) # to display all the data types
+print(df.dtypes) # displays all the data types
 
 # Handles any missing data
 print("\nMissing Values Before Handling:")
 print(df.isnull().sum()) # counts all the missing data
 
 # Remove duplicated data
-print("\nNumber of Duplicates Before Removal:", df.duplicated().sum()) # checks and counts the duplicated files before duplicate deletion
-df.drop_duplicates(inplace=True) # Removes the duplicated files
-print("Number of Duplicates After Removal:", df.duplicated().sum()) # checks and counts the duplicated files after duplicate deletions
+print("\nNumber of Duplicates Before Removal:", df.duplicated().sum()) # checks and counts the duplicates before duplicate deletion
+df.drop_duplicates(inplace=True) # Removes the duplicates
+print("Number of Duplicates After Removal:", df.duplicated().sum()) # checks and counts the duplicates after duplicate deletions
 
 # Descriptive statistics
 print("\nDescriptive Statistics:")
@@ -34,7 +34,7 @@ column_x = 'Gender' #can use any text based category/column
 column_y = 'Age' #can use any number based category/column
 plt.figure(figsize=(10, 6))
 sns.boxplot(x=df[column_x], y=df[column_y])  
-plt.title('Ages of people that go to the gym by gender')
+plt.title('Ages comparision between genders in the gym')
 plt.xlabel(column_x)
 plt.ylabel(column_y)
 plt.show()
